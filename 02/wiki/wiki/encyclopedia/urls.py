c6c8795a1496d56665fcdcb49e1b314f58435098
MohammadAli Abedi <ha13ed@yahoo.com>
2023-02-26 12:00:27 +0330

    Homework1 sent

--- 02/wiki/wiki/encyclopedia/urls.py
+++ 02/wiki/wiki/encyclopedia/urls.py
@@ -5,5 +5,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:q>", views.entry, name="entry"),
+    path("new_page", views.new_page, name="new_page"),
+    path("random_page", views.random_page, name="random_page"),
+    path("wiki/<str:q>", views.entry, name="entry"),
+    path("edit/<str:q>", views.edit_page, name="edit_page"),
 ]
--- 02/wiki/wiki/encyclopedia/views.py
+++ 02/wiki/wiki/encyclopedia/views.py
@@ -1,12 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import util
 import markdown2
 from django import forms
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from random import choice
 
 
-class NewQueryForm(forms.Form):
-    Query = forms.CharField()
-
+class NewPageForm(forms.Form):
+    title = forms.CharField(label='Title',widget=forms.TextInput(attrs={'placeholder': 'The title'}))
+    text = forms.CharField(label='', widget=forms.Textarea(attrs={'rows': 10, 'cols': 150,
+    'placeholder': 'The markdown', 'style': 'height: auto;'}))
 
 def index(request):
     msg=request.GET.get('q')
@@ -24,15 +28,60 @@ def index(request):
         })
 
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries(), "form": NewQueryForm()
+        "entries": util.list_entries()
     })
-
 
 def entry(request, q):
+    print("hi0",q)    
     if util.get_entry(q):
         return render(request, "encyclopedia/entry.html", {
-            "entry": markdown2.markdown(util.get_entry(q)), "title": q.capitalize()
+            "entry": markdown2.markdown(util.get_entry(q)), "title": q
         })
-    return render(request, "encyclopedia/entry.html", {
-        "entry": "Page Was Not Found", "title": "Page Not Found!"
+    return render(request, "encyclopedia/error.html", {
+        "title": q
     })
+
+def new_page(request):
+    if request.method == "POST":
+        form = NewPageForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            text = form.cleaned_data["text"]
+            if title.lower() in (entries.lower() for entries in util.list_entries()):
+                return render(request, "encyclopedia/error_new.html", {
+                    "title": title
+                })
+            util.save_entry(title, text)
+            return redirect(f"wiki/{title}")
+
+    form = NewPageForm()
+    return render(request, "encyclopedia/new.html", {
+        "form": form
+    })
+
+def edit_page(request, q):
+    print("hi1",q)
+    if request.method == "POST":
+        form = NewPageForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            text = form.cleaned_data["text"]
+            util.save_entry(title, text)
+            return redirect(f"../wiki/{title}")
+    if util.get_entry(q):
+        form = NewPageForm()
+        form.fields["title"].initial = q
+        form.fields["text"].initial = util.get_entry(q)
+        form.fields['title'].widget = forms.HiddenInput()
+        return render(request, "encyclopedia/edit.html", {
+            "form": form, "title": q
+        })
+    else:
+        return render(request, "encyclopedia/error.html", {
+            "title": q
+        })
+
+def random_page(request):
+    pages = util.list_entries()
+    page = choice(pages)
+    return redirect(f"wiki/{page}")
